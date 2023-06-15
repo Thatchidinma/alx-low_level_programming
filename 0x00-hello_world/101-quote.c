db5da60c9e0291704e3a990086f48fb85a0d299a
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 14:17:11 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * fwrite: prints out to the standard error
  * Return: end program
  */
-int main( )
+int main(void)
 {
 	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
 }
