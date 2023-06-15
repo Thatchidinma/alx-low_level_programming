a268bdce76ca01020949cfe79811ea1058836b07
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 15:05:38 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - contains all the blocks of code
- * fwrite: prints out to the standard error
+ * fputs: prints out to the standard error
  * Return: end program
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n");
+	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
