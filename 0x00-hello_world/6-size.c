8953b867c219217a54f4fb024a5c2f95d3951ce8
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 07:43:38 -0700

    types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - contains all the block of code
+ * printf: prints outs the types 
+ * Return: ends program
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSiz
+		e of a float: 4 byte(s)");
+	return (0);
+}
