720e4a04434a1642c39489428fdf744e134ab5ea
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 13:58:29 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - contains all the blocks of code
+ * fwrite: prints out to the standard error
+ * Return: end program
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (0);
+}
