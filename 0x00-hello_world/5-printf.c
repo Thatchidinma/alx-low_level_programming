bae808ff0e1edb56bf4d0547bf0af6b745c23356
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 06:16:14 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - contains block of codes
+ * printf: prints out statement
+ * Return: end program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
