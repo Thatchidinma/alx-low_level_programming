0a8b792915777b2420ea29a5eee88344ac79cea2
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 13:21:14 -0700

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of a char:%2d byte(s)", sizeof(char));
-	printf("Size of an int:%2d byte(s)", sizeof(int));
-	printf("Size of a long int:%2d byte(s)", sizeof(long int));
-	printf("Size of a long long int:%2d byte(s)", sizeof(long long int));	
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
 	return (0);
 }
