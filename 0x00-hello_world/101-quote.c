eae90e00e664e242359fef5f73e2315fbcc8a405
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 15:55:38 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  * main - contains all the blocks of code
- * fwrite: prints out to the standard error
+ * write: prints out to the standard error
  * Return: end program
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar,", stderr);
-	fwrite(" 2015-10-19\n", stderr);
+	write("and that piece of art is useful\" - Dora Korpar,", stderr);
+	write(" 2015-10-19\n", stderr);
 	return (1);
 }
