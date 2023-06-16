581fa2be645bdffc66429c1502fc7f1edc623bde
thatchidinma <ndukubachidinma@gmail.com>
2023-06-15 20:17:24 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 19);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
