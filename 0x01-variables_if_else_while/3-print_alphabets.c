68bd0d2180c00beb2c0a7ae054f424ab5d84d4b8
thatchidinma <ndukubachidinma@gmail.com>
2023-06-16 16:28:24 -0700

    print alphabet a-z and A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - contains all block of code
+ * for loop: satisfy condition
+ * printf: print out alphabets a-z
+ * Return: always return 0
+ */
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+        }
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+        putchar('\n');
+        return (0);
+}
