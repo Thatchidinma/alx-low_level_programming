674ab9316dd734bed55457902ef58446f8940ab3
thatchidinma <ndukubachidinma@gmail.com>
2023-06-16 17:07:20 -0700

    excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - contains all block of code
+ * for: satisfy conditions
+ * Return: always return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
