a12f6d6ba61aa22b905f4b7674bdde8c57ac32b4
thatchidinma <ndukubachidinma@gmail.com>
2023-06-20 18:45:03 -0700

    positive, negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - show if positive or negetive
+ *
+ * @n: int
+ *
+ * Return: 1 if positive 0 if 0 and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
