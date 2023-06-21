866498aed882605c1ce71650fd524128c10fb4e5
thatchidinma <ndukubachidinma@gmail.com>
2023-06-20 19:25:26 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print out last digit
+ *
+ * @n: int
+ *
+ * Return: always 0
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	return (m);
+}
