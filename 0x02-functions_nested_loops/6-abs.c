d5cb6b6c141995ccfb9e53dd7836249bede836bd
thatchidinma <ndukubachidinma@gmail.com>
2023-06-20 19:05:07 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - print absolute value
+ *
+ * Return: always 0
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
