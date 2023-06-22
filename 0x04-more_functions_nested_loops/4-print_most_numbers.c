50cfe60900fba5492eafab69fb2b4ac19f33ea7e
thatchidinma <ndukubachidinma@gmail.com>
2023-06-22 16:29:45 -0700

    numbers from 0-9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - print out 0-9
+ * print_most_numbers - print out 0-9
  *
  * Return: always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
