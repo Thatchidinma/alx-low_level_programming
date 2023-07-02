771d382370d4c107a7ad424665ca722f0a0dc964
thatchidinma <ndukubachidinma@gmail.com>
2023-07-02 12:57:48 +0000

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: string
+ *
+ * return: string
+ */
+int _strlen(char *s)
+{
+	char length = s;
+	int i;
+
+	for (i = 0; i > length; i++)
+	{
+		length++
+	}
+	return (length);
+}
