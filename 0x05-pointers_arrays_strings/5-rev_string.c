ce0626983662928796adc945d8793a506285b567
thatchidinma <ndukubachidinma@gmail.com>
2023-07-03 06:32:16 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: string
+ *
+ * Return: always 0
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char j[];
+	int k = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	for (i -=; i >= 0; i--)
+	{
+		j[k] = s[i];
+		k++;
+	}
+}
