7e5262ee061a62ee0312f94c5ce0d2f05b72cd0f
thatchidinma <ndukubachidinma@gmail.com>
2023-07-04 15:25:56 +0000

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,30 +9,31 @@
  */
 void puts_half(char *str)
 {
-	int strlen = 0, strlenhalfeven, strlenhalfodd, even;
+	int strlen = 0, strlenhalf, even;
 
 	while (str[strlen] != '\0')
 	{
 		strlen++;
 	}
-	strlenhalfeven = strlen / 2;
-	strlenhalfodd  = (strlen - 1) / 2;
+
 	even = strlen % 10;
 	if (even == 0)
 	{
-		while (str[strlenhalfeven] < str[strlen])
+		strlenhalf = strlen / 2;
+		while (strlenhalf < strlen)
 		{
-			_putchar(str[strlenhalfeven]);
-			strlenhalfeven++;
+			_putchar(str[strlenhalf - 1]);
+			strlenhalf++;
 		}
 		_putchar('\n');
 	}
 	else
 	{
-		while (str[strlenhalfodd] < str[strlen])
+		strlenhalf = (strlen - 1) / 2;
+		while (strlenhalf < strlen)
 		{
-			_putchar(str[strlenhalfodd]);
-			strlenhalfodd++;
+			_putchar(str[strlenhalf - 1]);
+			strlenhalf++;
 		}
 	}
 }
