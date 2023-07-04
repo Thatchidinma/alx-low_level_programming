5fda2570392ce118036b3bf8e11c03bdb5eeefc3
thatchidinma <ndukubachidinma@gmail.com>
2023-07-04 15:07:52 +0000

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of the string
+ *
+ * @str: string
+ *
+ * Return: always 0
+ */
+void puts_half(char *str)
+{
+	int strlen = 0, strlenhalfeven, strlenhalfodd;
+
+	while (str[strlen] != '\0')
+	{
+		strlen++;
+	}
+	strlenhalfeven = strlen / 2;
+	strlenhalfodd  = (strlen - 1) / 2;
+	if ( strlen % 10 = 0)
+	{
+		while (str[strlenhalfeven] < str[strlen])
+		{
+			_putchar(str[strlenhafeven]);
+			strlenhalfeven++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		while (str[strlenhalfodd] < str[strlen])
+		{
+			_putchar(str[strlenhalfodd]);
+			strlenhalfodd++;
+		}
+	}
+}
