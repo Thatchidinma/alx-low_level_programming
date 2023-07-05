129b5a883716061d7b4e125552037f0cccddafae
thatchidinma <ndukubachidinma@gmail.com>
2023-07-05 18:41:50 +0000

    reverse with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print in reverse with recursion
+ *
+ * @s: string
+ *
+ * Return: always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i++;
+	}
+	if (i < 1)
+	{
+		_putchar(s[i]);
+		i--;
+		_print_rev_recursion(s[i]);
+	}
+	else
+	{ 
+		_putchar('\n')
+	}
+}
