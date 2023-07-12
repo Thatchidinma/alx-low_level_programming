bf23f331ca4acd2f4aac7cc5e5b624424639ef1a
thatchidinma <ndukubachidinma@gmail.com>
2023-07-12 20:11:54 +0000

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(atoi(argv[i])) && atoi(argv[i]) > 0)
+			if (isdigit(argv[i]) && atoi(argv[i]) > 0)
 			{
 				sum += atoi(argv[i]);
 			}
