44286b4172606d08d9c805fbec440fc4a5653bd7
thatchidinma <ndukubachidinma@gmail.com>
2023-07-14 19:21:33 +0000

    string concentration

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concentrates to strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * @n: number
+ *
+ * Return: always 0
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *c[] = malloc(s1);
+
+	while (*s1 != '\0')
+	{
+		s1++;
+	}
+	while (*s2 != s[n])
+	{
+		*s1 = *s2;
+		*s1++;
+		*s2++;
+	}
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+}
