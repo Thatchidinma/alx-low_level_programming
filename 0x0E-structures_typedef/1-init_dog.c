99cb78ef707a6f3c0ef8252866b0169d0e322af4
thatchidinma <ndukubachidinma@gmail.com>
2023-07-17 12:43:13 +0000

    initilizing strutures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize struct dog
+ *
+ * @d: pointer to structure
+ *
+ * @name: its name
+ *
+ * @age: its age
+ *
+ * @owner: its owner
+ *
+ * Return: always 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
