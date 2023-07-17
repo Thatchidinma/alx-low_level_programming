5862da65c917863db8fb6fca1a655c336c8d03e7
thatchidinma <ndukubachidinma@gmail.com>
2023-07-17 15:10:33 +0000

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,6 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-
 		if (d->age > 0)
 		{
 			printf("Age: %f\n", d->age);
@@ -28,7 +27,6 @@ void print_dog(struct dog *d)
 		{
 			printf("Age: (nil)\n");
 		}
-
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
