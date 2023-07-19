ddcd6d42852ae10512e0e5d8bf3089c9b944d7a2
thatchidinma <ndukubachidinma@gmail.com>
2023-07-19 07:38:26 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints out name
+ *
+ * @name: name to be printed out
+ *
+ * @f: pointer to a function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	for (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
