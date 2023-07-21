8c2f15941c4f2128a78cd936be27b4014f3dc3c6
thatchidinma <ndukubachidinma@gmail.com>
2023-07-21 21:51:32 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@
  *
  * Return: nothing
  */
-void print_numbers(const char *separator, const unsigned int n, ...)i
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i = 0;
