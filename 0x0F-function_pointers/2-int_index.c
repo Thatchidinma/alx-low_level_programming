6e56a09c641d36c8998b439cd8aea23fdb0b26ae
thatchidinma <ndukubachidinma@gmail.com>
2023-07-21 19:14:01 +0000

    search

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,19 +15,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size > 0)
+	if (array != NULL && cmp != NULL)
 	{
-		if (array != NULL && cmp != NULL)
+		while (i < size)
 		{
-			while (i < size)
+			if (cmp(array[i]))
 			{
-				if (cmp(array[i]))
-				{
-					return (i);
-				}
-				i++;
+				return (i);
 			}
+
+			i++;
 		}
 	}
+
 	return (-1);
 }
