47d87d295c3c5be945ebef125b3fca1a838e28c3
thatchidinma <ndukubachidinma@gmail.com>
2023-08-02 00:18:36 +0000

    free list and set head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -18,9 +18,10 @@ void free_listint2(listint_t **head)
 	 }
 	 while (head != NULL)
 	 {
-		 temp = (*head)->next;
-		 free(*head);
-		 *head = temp;
+		 temp = (*head);
+		 *head = (*head)->next;
+		 free(temp);
 	 }
+	 free(*head);
 	 head = NULL;
 }
