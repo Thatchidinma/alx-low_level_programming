0d1be17442ba2ad6e8db79695d1f020975521ce0
thatchidinma <ndukubachidinma@gmail.com>
2023-08-21 22:31:50 +0000

    print in binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary rep of a number
+ *
+ * @n: the number
+ *
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	_divide(n);
+}
+
+/**
+ * _divide - divides
+ *
+ * @n: long int
+ *
+ * Return: 0
+ */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+	{
+		return;
+	}
+	_divide(n >> 1);
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
